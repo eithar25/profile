000bba43b5f0c95ea0eeb905e9a2f7b06f8c96ed
eithar25 <131480817+eithar25@users.noreply.github.com>
2024-02-16 20:16:28 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,19 +1,49 @@
-#include <iostream>
-#include <string>
-using namespace std;
 
+
+#include <bits/stdc++.h>
+using namespace std;
+class passport
+{
+public:
+    string first_name;
+    int password;
+    string Firts_name, second_name, city_name,school_name;
+    int year_of_birth;
+    passport()
+    {
+        cout<<"enter your first name"<<"\t";
+        cin>>first_name;
+        cout<<"enter your password"<<"\t";
+        cin>>password;
+        if (password==123456789)
+           cout<<"enter your first name"<<"\t";
+           cin>>Firts_name;
+           cout<<"enter your second name"<<"\t";
+           cin>>second_name;
+           cout<<"enter your city name"<<"\t";
+           cin>>school_name;
+           cout<<"enter your school name"<<"\t";
+           cin>>year_of_birth;
+           cout<<"enter your year of birth"<<"\t";
+            cin>>Firts_name>> second_name>> city_name>>school_name>>year_of_birth;
+           
+        else
+            cout<<"password is not correct";
+    
+    void information()
+    {
+        cout<<Firts_name<<"\t"<< second_name<<"\t"<<city_name<<"\t"<<school_name<<"\t"<<year_of_birth<<"\t";
+    }};
 int main()
 {
-    int math= 15, chemistry= 23, physics=16, geology=26;
-    double biology=30.8, mechanics=19.5;
-    double final_score= math+ chemistry+ physics+ geology+ biology+ mechanics;
-    if (final_score>120)
-        cout<<"your score is the highest";
-
-else if (final_score<120)
-
-cout<< "your score is the lowest";
-
-cout<< (math<physics)&&(geology>biology);
-return 0;
+    passport id;
+    id.information();
+    int age;
+    int current_year=2022;
+    int year_of_birth;
+    cout<<"enter your year of birth"<<"\t";
+    cin>>year_of_birth;
+    age= 2022 - year_of_birth;
+    cout<<age;
+    return 0;
 }
