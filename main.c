fe3bced4216f445b65045c197ef22a9137c47f86
eithar25 <131480817+eithar25@users.noreply.github.com>
2024-02-16 20:19:47 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,31 +3,7 @@
 
 int main()
 {
-   int n;//all the cubes
-   int x;//nom. of levels
-   int l=2l;//one level
-   scanf("%d",&n);
-   for(int l=1;0<l<n;l++){
-  for(int x=2;0<x<n;l++) ;
-   };
-   return 0;
-}
-
-
-
-
-
-#include <stdio.h>
-#include <stdlib.h>
-
-int main()
-{
-
- int N, count=0;
- scanf("%d",&N);
- for(int N;1<=N<=100;N++){
-    count+=N;
- }
-  printf("%d",count);
-   return 0;
+   int x=5 , y=8, result=x+y;
+    printf("%d",result);
+    return 0;
 }
