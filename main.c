9ddef3ce43b44620a959235e92bcd014601f77fc
eithar25 <131480817+eithar25@users.noreply.github.com>
2024-02-16 20:20:07 +0200

    Add files via upload

--- main.c
+++ main.c
@@ -3,7 +3,13 @@
 
 int main()
 {
-   int x=5 , y=8, result=x+y;
-    printf("%d",result);
+int n;
+ scanf("%d",&n);
+ if (n=n+2)
+        printf("white");
+
+  else if(n=n+1)
+    printf("black");
     return 0;
-}
+    }
+
