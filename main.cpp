936d3e875a1e8a0b26865d0197aa84c3fc0df518
eithar25 <131480817+eithar25@users.noreply.github.com>
2024-02-16 20:16:50 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,49 +1,21 @@
-
-
-#include <bits/stdc++.h>
+#include <iostream>
 using namespace std;
-class passport
-{
+
+class student{
 public:
-    string first_name;
-    int password;
-    string Firts_name, second_name, city_name,school_name;
-    int year_of_birth;
-    passport()
-    {
-        cout<<"enter your first name"<<"\t";
-        cin>>first_name;
-        cout<<"enter your password"<<"\t";
-        cin>>password;
-        if (password==123456789)
-           cout<<"enter your first name"<<"\t";
-           cin>>Firts_name;
-           cout<<"enter your second name"<<"\t";
-           cin>>second_name;
-           cout<<"enter your city name"<<"\t";
-           cin>>school_name;
-           cout<<"enter your school name"<<"\t";
-           cin>>year_of_birth;
-           cout<<"enter your year of birth"<<"\t";
-            cin>>Firts_name>> second_name>> city_name>>school_name>>year_of_birth;
-           
-        else
-            cout<<"password is not correct";
-    
-    void information()
-    {
-        cout<<Firts_name<<"\t"<< second_name<<"\t"<<city_name<<"\t"<<school_name<<"\t"<<year_of_birth<<"\t";
-    }};
+int age;
+string name;
+string school;
+
+};
+
+
 int main()
 {
-    passport id;
-    id.information();
-    int age;
-    int current_year=2022;
-    int year_of_birth;
-    cout<<"enter your year of birth"<<"\t";
-    cin>>year_of_birth;
-    age= 2022 - year_of_birth;
-    cout<<age;
+    student Eithar;
+    Eithar.age = 16;
+    Eithar.name="Eithar";
+    Eithar.school="STEM";
+    cout<<Eithar.name <<Eithar.age<<Eithar.school;
     return 0;
 }
