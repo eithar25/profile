e1519bfa86820a950dda7dbe4c6b03af3cb0b624
eithar25 <131480817+eithar25@users.noreply.github.com>
2024-02-16 20:18:01 +0200

    Add files via upload

---
+++ sketch_dec27a.ino
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main()
+{
+  int mark=(90,89,88,87,86,85,84,83,82,82,71,70,69,68,67,66,65,64,63,62,61,60,59,58,57,56,55,54,53,52,51)
+  scanf("%d",&90);
+  scanf("%d",&75);
+  scanf("%d",&50);
+  if (mark>=90){
+    printf("A");
+  }
+else if (mark<90&&mark>75){
+  printf("B")
+}
+else if(mark<75&&mark>50){
+  printf("c");
+}
+}
