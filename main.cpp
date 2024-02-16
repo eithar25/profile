952f7072b6ac3ee99b247bebdef615dedee1f169
eithar25 <131480817+eithar25@users.noreply.github.com>
2024-02-16 20:18:52 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,20 +1,15 @@
 #include <iostream>
 
 using namespace std;
+class myclass;
+private:
+    int w;
+};
 
 int main()
 {
-   try{
-    int father, son;
-    cin>>father>>son;
-    cout<<father,son;
-    if (father>son){
-        cout<<"these ages are possible";
-    }
-    }
-   throw 404;
-else(){
-  catch (...)
-  cout<<"son can't be older than father"
-   }
+  myclass mynom;
+  mynom.w=5;
+  cout<<mynom.w;
+    return 0;
 }
