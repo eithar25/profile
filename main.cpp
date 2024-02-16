34f7e31ece113f79353331ecf0ce30e491ca98e7
eithar25 <131480817+eithar25@users.noreply.github.com>
2024-02-16 20:14:12 +0200

    Add files via upload

--- main.cpp
+++ main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
-
+#include <string>
 using namespace std;
 
 int main()
 {
-    int N;
-     cin >> N >> endl;
-    string =s;
-    cin >> s >> endl;
-    if s[N-1=='x']  {cout<<"No" << endl;}
- else {
-    cout << "yes" <<endl;
- }
+    int math= 15, chemistry= 23, physics=16, geology=26;
+    double biology=30.8, mechanics=19.5;
+    double final_score= math+ chemistry+ physics+ geology+ biology+ mechanics;
+    if (final_score>120)
+        cout<<"your score is the highest";
+
+else if (final_score<120)
+
+cout<< "your score is the lowest";
+
+cout<< (math<physics)&&(geology>biology);
+return 0;
 }
